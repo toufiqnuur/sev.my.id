bde2dac25dd2c841eb5cf88803cf28e8219d92e6
toufiqnuur <taufik2017yk@gmail.com>
2023-01-26 17:57:56 +0700

    init: supabase and chakraui

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,25 @@
-import '@/styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [supabaseClient] = useState(() =>
+    createBrowserSupabaseClient({
+      cookieOptions: {
+        maxAge: 604800,
+      },
+    })
+  );
+
+  return (
+    <ChakraProvider>
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+      >
+        <Component {...pageProps} />
+      </SessionContextProvider>
+    </ChakraProvider>
+  );
 }
