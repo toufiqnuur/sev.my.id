47007dcf7c7f0c0dc79978e31985942d2599fcbd
toufiqnuur <taufik2017yk@gmail.com>
2023-06-19 18:24:30 +0700

    fix redirect bug

--- src/pages/[short]/index.js
+++ src/pages/[short]/index.js
@@ -9,15 +9,23 @@ export default function ShortPage({ page }) {
   return (
     <>
       <Container maxW="container.xl" px={[4, 10]}>
-        <Box h={24} display="flex" alignItems="center" justifyContent="space-between">
+        <Box
+          h={24}
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+        >
           <Image src="/logo.png" width={160} height={80} alt="" />
           <Button colorScheme="twitter" size="lg" rounded="xl">
             Make your own
           </Button>
         </Box>
 
-        {page?.protected && !page?.expired?.state && <ProtectedLink urlId={page?.id} />}
-        {page?.expired?.state && <ExpiredLink expired={page?.expired} />}
+        {page.expired.state ? (
+          <ExpiredLink expired={page.expired} />
+        ) : page.protected ? (
+          <ProtectedLink urlId={page.id} />
+        ) : null}
       </Container>
     </>
   );
@@ -27,14 +35,27 @@ export async function getServerSideProps(ctx) {
   const supabaseServer = createServerSupabaseClient(ctx);
   const { short } = ctx.params;
 
-  const { data } = await supabaseServer.from("urls").select().eq("slug", short).single();
+  const { data } = await supabaseServer
+    .from("urls")
+    .select()
+    .eq("slug", short)
+    .single();
 
-  const updateViewsTable = async () => {
-    const { data: views } = await supabaseServer.from("views").select().single();
+  const updateViews = async () => {
+    // update views
+    await supabaseServer
+      .from("urls")
+      .update({ hit: data.hit++ })
+      .eq("id", data.user_id);
+    // update analytics
+    const { data: views } = await supabaseServer
+      .from("views")
+      .select()
+      .single();
     if (views) {
       await supabaseServer
         .from("views")
-        .update({ count: data.count + 1 })
+        .update({ count: data.count++ })
         .eq("date", dayjs().format("YYYY-MM-DD"))
         .eq("url_id", data.id);
     } else {
@@ -49,43 +70,38 @@ export async function getServerSideProps(ctx) {
 
   if (data) {
     const isExpired = dayjs(data.expired_at).valueOf() < dayjs().valueOf();
+    const isProtected = !!data.secret_key;
 
+    // measure owner is not ANONYMOUS
     if (data.user_id) {
-      // update hit column in urls table
-      await supabaseServer
-        .from("urls")
-        .update({ hit: data.hit + 1 })
-        .eq("id", data.id);
+      updateViews();
+    }
 
-      // update analytics table
-      updateViewsTable();
-
-      if (isExpired || data.secret_key) {
-        return {
-          props: {
-            page: {
-              id: data.id,
-              expired: {
-                state: isExpired,
-                date: data.expired_at,
-              },
-              protected: Boolean(data.secret_key),
+    if (isExpired || isProtected) {
+      return {
+        props: {
+          page: {
+            id: data.id,
+            protected: isProtected,
+            expired: {
+              state: isExpired,
+              date: data.expired_at,
             },
           },
-        };
-      } else {
-        return {
-          notFound: false,
-          redirect: {
-            destination: data.real_url,
-            permanent: true,
-          },
-        };
-      }
+        },
+      };
+    } else {
+      return {
+        notFound: false,
+        redirect: {
+          destination: data.real_url,
+          permanent: true,
+        },
+      };
     }
+  } else {
+    return {
+      notFound: true,
+    };
   }
-  return {
-    notFound: true,
-    props: {},
-  };
 }
